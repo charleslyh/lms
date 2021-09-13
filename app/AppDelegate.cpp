9c034777185e554e1fc2d4eaaac8c7465f618022
charles <charles.liyh@gmail.com>
2021-09-14 03:08:17 +0800

    VideoFile实现视频基本信息输出

--- app/AppDelegate.cpp
+++ app/AppDelegate.cpp
@@ -1,16 +1,26 @@
 #include "LMSFoundation/Foundation.h"
 #include "LMSFoundation/Player.h"
 #include "SDLApplication.h"
-
+#ifdef _WIN32
+  #include <direct.h>
+  #define getcwd _getcwd // stupid MSFT "deprecation" warning
+#else
+  #include <unistd.h>
+  #include <limits.h>
+#endif
 
 class PlayerAppDelegate: public SDLAppDelegate, public lms::DispatchQueue {
 public:
   void didFinishLaunchingApplication(int argc, char **argv) override {
+    char path[PATH_MAX] = {0};
+    getcwd(path, PATH_MAX);
+    printf("Current Working Directory: %s\n", path);
+
     lms::init({
       this
     });
 
-    lms::VideoFile *src = lms::autoRelease(new lms::VideoFile("path/to/file"));
+    lms::VideoFile *src = lms::autoRelease(new lms::VideoFile(argv[1]));
     player = new lms::Player(src);
     player->play();
   }
--- lms/LMSFoundation/Player.h
+++ lms/LMSFoundation/Player.h
@@ -1,17 +1,58 @@
-#include <cstdio>
 #include "LMSFoundation/Foundation.h"
+
+extern "C" {
+#include <libavformat/avformat.h>
+}
+
+#include <cstdio>
+#include <string>
 
 namespace lms {
 
-class VideoFile: public Object {
+class StreamSource {
+public:
+  virtual void open() = 0;
+  virtual void close() = 0;
+};
+
+class VideoFile: public Object, public StreamSource {
 public:
   VideoFile(const char *path) {
     printf("VideoFile::VideoFile(\"%s\"): %p\n", path, this);
+    this->path = strdup(path);
   }
 
   ~VideoFile() {
+    free(this->path);
     printf("VideoFile::~VideoFile(): %p\n", this);
   }
+
+  void open() override {
+    context = nullptr;
+    int rt = 0;
+
+    rt = avformat_open_input(&context, path, nullptr, nullptr);
+    if (rt != 0) {
+      printf("Failed opening video file!\n");
+      return;
+    }
+
+    rt = avformat_find_stream_info(context, nullptr);
+    if (rt != 0) {
+      printf("Failed finding stream info in video file!\n");
+      return;
+    }
+    
+    av_dump_format(context, 0, path, 0);
+  }
+
+  void close() override {
+    avformat_close_input(&context);
+  }
+
+private:
+  AVFormatContext *context;
+  char *path;
 };
 
 
